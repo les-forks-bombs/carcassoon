d8bbd847e42bb050a19985400a8acdfcb2183811
Matthieu Pignolet <matthieu@puffer.fish>
2026-04-05 13:15:34 +0200

    feat(libcarcassonne/test): ajoute de vérifications

--- lib/libcarcassonne/tests/deck.c
+++ lib/libcarcassonne/tests/deck.c
@@ -23,28 +23,31 @@ void deck_seed_consistent(void **state)
     deck_t deck3 = create_deck(100);
 
     // we skip the first pick because it's always the starting one
-    pick(&deck1);
-    pick(&deck2);
-    pick(&deck3);
+    
+    tile_t *r = deck_pick(&deck1);
+    assert_string_equal(deck_pick(&deck2)->name, r->name);
+    assert_string_equal(deck_pick(&deck3)->name, r->name);
 
-    int i = 0;
+    int i = 1; // set to one because we skipped the first one
     while (deck1.list.head != NULL)
     {
-        tile_t *r = pick(&deck1);
+        r = deck_pick(&deck1);
 
         // on vérifie que deux générateurs doivent être égals
-        assert_string_equal(r->name, pick(&deck2)->name);
+        assert_string_equal(r->name, deck_pick(&deck2)->name);
 
         // au début, on vérifie que nos générateurs diffèrent bien
         if (i < 1)
-            assert_string_not_equal(r->name, pick(&deck3)->name);
+            assert_string_not_equal(r->name, deck_pick(&deck3)->name);
 
         i++;
     }
 
-    tile_t *r = pick(&deck1);
-    assert_ptr_equal(r, pick(&deck2));
+    r = deck_pick(&deck1);
+    assert_ptr_equal(r, deck_pick(&deck2));
     assert_null(r);
+
+    assert_int_equal(i, 72);
 
     free_deck(deck1);
     free_deck(deck2);
