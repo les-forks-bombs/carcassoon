e8811ce813cb1c58fee1e1b49da61fb33c53284a
Matthieu Pignolet <matthieu@puffer.fish>
2026-04-05 13:32:44 +0200

    feat(docs): ajout de documentation & features doxygen

--- lib/libcarcassonne/engine.h
+++ lib/libcarcassonne/engine.h
@@ -2,11 +2,15 @@
 #define H_LIBCARCASSONNE_ENGINE
 
 #include <libcarcassonne/game.h>
+#include <libcarcassonne/deck.h>
 
+/// @brief Représente un moteur pour une partie de carcassonne
 typedef struct engine
 {
+    /// @brief La statut du plateau de jeu
     game_t game;
+    /// @brief Le deck de cartes initialisé
+    deck_t deck;
 } engine_t;
-
 
 #endif
