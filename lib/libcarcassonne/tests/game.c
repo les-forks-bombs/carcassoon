e01b17dc3d3ecbb6ef4936962962968ffa009f27
= <=>
2026-04-05 17:29:30 +0200

    création des fonctions pour print et placer une tuile, à tester

---
+++ lib/libcarcassonne/tests/game.c
@@ -0,0 +1,10 @@
+#include <setjmp.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#include <cmocka.h>
+
+#include <libcarcassonne/game.h>
+#include <libcarcassonne/tests/tests.h>
+
