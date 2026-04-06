f57ca1b353e0039a2f05bae4097bac793f5eb754
Matthieu Pignolet <matthieu@puffer.fish>
2026-04-07 00:26:43 +0200

    feat(libcarcassonne/game): changements dans l'interface et implémentation

--- lib/libcarcassonne/game.c
+++ lib/libcarcassonne/game.c
@@ -1,87 +1,122 @@
-#include "game.h"
+#include <libcarcassonne/game.h>
 
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <memory.h>
 #include <libcarcassonne/consts.h>
 
-game_t create_game(int nb_players, int nb_ai, int seed, int max_turns)
+return_code_t create_game(
+    game_t *game,
+    unsigned int players_count,
+    unsigned int ai_count,
+    unsigned int seed,
+    unsigned int turns_limit)
 {
-    assert(nb_players >= 2 && nb_players <= 5);
-    assert(nb_ai >= 0 && nb_ai <= nb_players);
-
-    game_t game = {
-        .deck = create_deck(seed),
-        .nb_players = nb_players,
-        .turn = 0,
-        .max_turn = max_turns,
-
-    };
-
-    for (int i = 0; i < nb_players; i++)
+    // On vérifie que le nombre de joueurs est dans [2, 5]
+    // et que le nombre d'IA est inférieur au nombre de joueurs
+    if (
+        players_count < 2 || players_count > 5 || ai_count > players_count)
     {
-        player_t p;
-        if (i < nb_players - nb_ai)
-        {
-            p = create_player(LIBCARCASSONNE_PLAYER_HUMAN);
-        }
-        else
-        {
-            p = create_player(LIBCARCASSONNE_PLAYER_AI);
-        }
-        game.players[i] = p;
+        return ERROR;
     }
 
-    game.current_player = &game.players[0];
+    game->current_player = 0;
+    game->deck = create_deck(seed);
+    game->players_count = players_count;
+    game->turn = 0;
+    game->turns_limit = turns_limit;
 
-    return game;
+    // todo: considérer les extensions pour calculer la taille max du tableau
+    unsigned int largeur = LIBCARCASSONNE_TILES_COUNT + LIBCARCASSONNE_TILES_COUNT - 1;
+
+    game->map = calloc(
+        largeur * largeur,
+        sizeof(placed_tile_t *));
+
+    // on instancie les joueurs
+    for (unsigned int i = 0; i < players_count; i++)
+        game->players[i] = create_player(
+            i > ai_count ? LIBCARCASSONNE_PLAYER_HUMAN : LIBCARCASSONNE_PLAYER_AI);
+
+    return SUCCESS;
 }
 
-void destroy_game(game_t game)
+void destroy_game(game_t *game)
 {
-
-    game.current_player = NULL;
-
-    free_deck(game.deck);
-
-    game.nb_players = 0;
-    game.turn = 0;
-    game.max_turn = 0;
-}
-
-bool place_tile(game_t *game, tile_t tile, int x,int y){
-    int zero = LIBCARCASSONNE_TILES_COUNT-1;
-    if (x<=-LIBCARCASSONNE_TILES_COUNT ||
-        y<=-LIBCARCASSONNE_TILES_COUNT ||
-        x>=LIBCARCASSONNE_TILES_COUNT ||
-        y>=LIBCARCASSONNE_TILES_COUNT){
-        return false;
-    }
-    if (game->map[calc_index(zero+y,zero+x)]==NULL){
-        game->map[calc_index(zero+y,zero+x)]=tile;
-        return true;
-    }
-    else {
-        return false;
-    }
-}
-
-int calc_index(int y,int x){
-    int size = LIBCARCASSONNE_TILES_COUNT*2-1;
-    return y*size+x;
-}
-
-void print_map(game_t game){
-    for(int i=0;i<LIBCARCASSONNE_TILES_COUNT*2-1;i++){
-        for(int j=0;j<LIBCARCASSONNE_TILES_COUNT*2-1;j++){
-            if (game.map[calc_index(i,j)]==NULL){
-                printf(".");
-            }
-            else {
-                printf("#");
-            }
+    // Cleanup des structures dans la map (les placed_tile_t)
+    for (int i = -LIBCARCASSONNE_TILES_COUNT + 1; i < LIBCARCASSONNE_TILES_COUNT; i++)
+        for (int j = -LIBCARCASSONNE_TILES_COUNT + 1; j < LIBCARCASSONNE_TILES_COUNT; j++)
+        {
+            placed_tile_t **tile = game_tile_at(game, i, j);
+            if (*tile != NULL)
+                free(*tile);
         }
-        printf("\n");
+
+    free(game->map);
+    free_deck(game->deck);
+}
+
+placed_tile_t **game_tile_at(
+    game_t *game,
+    int colonne,
+    int ligne)
+{
+    int zero = LIBCARCASSONNE_TILES_COUNT - 1;
+    int size = LIBCARCASSONNE_TILES_COUNT * 2 - 1;
+
+    int index = (zero + ligne) * size + (zero + colonne);
+
+    return &game->map[index];
+}
+
+return_code_t game_place_tile(
+    game_t *game,
+    tile_t *tile,
+    int x,
+    int y)
+{
+    if (x <= -LIBCARCASSONNE_TILES_COUNT ||
+        y <= -LIBCARCASSONNE_TILES_COUNT ||
+        x >= LIBCARCASSONNE_TILES_COUNT ||
+        y >= LIBCARCASSONNE_TILES_COUNT)
+    {
+        return -2; // Out of bounds
+    }
+
+    placed_tile_t **tile_ref = game_tile_at(game, x, y);
+
+    if (*tile_ref == NULL)
+    {
+        placed_tile_t *placed_tile = calloc(1, sizeof(placed_tile_t));
+        placed_tile->parent = tile;
+
+        // todo: implémenter les meeple
+        placed_tile->meeple = NULL;
+
+        *tile_ref = placed_tile;
+        return 1; // Placed
+    }
+    else
+    {
+        return -1; // not Free
+    }
+}
+
+void game_print_map(game_t *game)
+{
+    for (int i = -LIBCARCASSONNE_TILES_COUNT + 1; i < LIBCARCASSONNE_TILES_COUNT; i++)
+    {
+        printf("|");
+        for (int j = -LIBCARCASSONNE_TILES_COUNT + 1; j < LIBCARCASSONNE_TILES_COUNT; j++)
+        {
+            placed_tile_t **tile = game_tile_at(game, i, j);
+            if (*tile == NULL)
+                printf(".");
+            else
+                printf("#");
+        }
+        printf("|\n");
     }
 }
