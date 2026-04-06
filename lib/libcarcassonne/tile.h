0df2a490244e91c6e2f87607e473ac6fbf358209
Matthieu Pignolet <matthieu@puffer.fish>
2026-04-07 00:24:01 +0200

    feat(libcarcassonne/{deck,tile}): changements dans la représentation mémoire, ajout du système de groupe, mais non complet (manque les définitions)

--- lib/libcarcassonne/tile.h
+++ lib/libcarcassonne/tile.h
@@ -2,6 +2,7 @@
 #define H_LIBCARCASSONNE_TILE
 
 #include <stdbool.h>
+#include <libcarcassonne/meeple.h>
 
 #define LIBCARCASSONNE_TILE_PART_VILLAGE 0
 #define LIBCARCASSONNE_TILE_PART_FIELD   1
@@ -10,17 +11,38 @@
 #define LIBCARCASSONNE_TILE_PART_ABBEY   4
 #define LIBCARCASSONNE_TILE_PART_TOWN    5
 
-typedef char tile_type_t;
+#define LIBCARCASSONNE_TILE_PART_A 1 << 0
+#define LIBCARCASSONNE_TILE_PART_B 1 << 1
+#define LIBCARCASSONNE_TILE_PART_C 1 << 2
+#define LIBCARCASSONNE_TILE_PART_D 1 << 3
+#define LIBCARCASSONNE_TILE_PART_E 1 << 4
+#define LIBCARCASSONNE_TILE_PART_F 1 << 5
+#define LIBCARCASSONNE_TILE_PART_G 1 << 6
+#define LIBCARCASSONNE_TILE_PART_H 1 << 7
+#define LIBCARCASSONNE_TILE_PART_I 1 << 8
 
-/// @brief A tile is a 3x3 of tile parts
+typedef char tile_part_type_t;
+typedef char tile_part_group_t;
+
+/// @brief Une tile est un élément de jeu de carcassonne
 typedef struct tile
 {
-    /// @brief The "name" of this tile
-    char name[5];
-    /// @brief Every tile is made of 9 tile parts
-    tile_type_t parts[9];
-
+    /// @brief Famille de la tile
+    char family[5];
+    /// @brief Parties de la tile
+    tile_part_type_t parts[9];
+    /// @brief Spécifie si la tile est un blason
     bool blason;
+    /// @brief Spécifie le nombre de tiles présentes
+    unsigned int amount;
+    /// @brief Définition des groupes a l'intérieur d'une tile
+    tile_part_group_t parts_groups[9];
 } tile_t;
+
+typedef struct placed_tile
+{
+    tile_t* parent;
+    meeple_t* meeple;
+} placed_tile_t;
 
 #endif
