bef2e6d50b57bd81e751833ca0785b985c7fd6e6
Matthieu Pignolet <matthieu@puffer.fish>
2026-04-12 01:16:03 +0200

    feat: lint and use a loop for finding the old child ref and change it to null + reflect the change

--- lib/libcarcassonne/tile.h
+++ lib/libcarcassonne/tile.h
@@ -1,18 +1,18 @@
 #ifndef H_LIBCARCASSONNE_TILE
 #define H_LIBCARCASSONNE_TILE
 
+#include <libcarcassonne/consts.h>
 #include <libcarcassonne/meeple.h>
 #include <stdbool.h>
-#include <libcarcassonne/consts.h>
 
 /// @brief Les différents types de sous-tiles
 enum tile_part_kind {
   LIBCARCASSONNE_TILE_PART_VILLAGE = 0,
-  LIBCARCASSONNE_TILE_PART_FIELD =   1,
-  LIBCARCASSONNE_TILE_PART_ROAD =    2,
-  LIBCARCASSONNE_TILE_PART_WALL =    3,
-  LIBCARCASSONNE_TILE_PART_ABBEY =   4,
-  LIBCARCASSONNE_TILE_PART_TOWN =    5
+  LIBCARCASSONNE_TILE_PART_FIELD   = 1,
+  LIBCARCASSONNE_TILE_PART_ROAD    = 2,
+  LIBCARCASSONNE_TILE_PART_WALL    = 3,
+  LIBCARCASSONNE_TILE_PART_ABBEY   = 4,
+  LIBCARCASSONNE_TILE_PART_TOWN    = 5
 };
 
 /// @brief Les sous-groupes possibles [0-7]
@@ -29,16 +29,17 @@ enum tile_part_group {
 
 /// @brief Les orientations possibles pour les tiles
 /// @remark Pour avoir l'opposé on a (n + 2) % 4
-/// @remark Pour avoir le suivant (sens des aiguilles d'une montre on à) (n + 1) % 4
+/// @remark Pour avoir le suivant (sens des aiguilles d'une montre on à) (n + 1)
+/// % 4
 enum tile_orientation {
   LIBCARCASSONNE_TILE_ORIENTATION_NORTH = 0,
-  LIBCARCASSONNE_TILE_ORIENTATION_EAST = 1,
+  LIBCARCASSONNE_TILE_ORIENTATION_EAST  = 1,
   LIBCARCASSONNE_TILE_ORIENTATION_SOUTH = 2,
-  LIBCARCASSONNE_TILE_ORIENTATION_WEST = 3
+  LIBCARCASSONNE_TILE_ORIENTATION_WEST  = 3
 };
 
-typedef enum tile_part_kind tile_part_type_t;
-typedef enum tile_part_group tile_part_group_t;
+typedef enum tile_part_kind   tile_part_type_t;
+typedef enum tile_part_group  tile_part_group_t;
 typedef enum tile_orientation tile_orientation_t;
 
 /// @brief Une tile est un élément de jeu de carcassonne
@@ -71,12 +72,12 @@ typedef struct placed_tile_group {
 /// @brief Représentation d'une tile qui a été placée
 typedef struct placed_tile {
   /// @brief Pointeur vers la définition de la tile
-  tile_t*              parent;
+  tile_t* parent;
   /// @brief Groupes présents a l'intérieur de la tile
   /// @remark On peut au maximum avoir 9 groupes dans une tile
   placed_tile_group_t* groups[9];
   /// @brief Orientation de placement de la tile
-  tile_orientation_t   orientation;
+  tile_orientation_t orientation;
 } placed_tile_t;
 
 /// @brief Récupère une famille de la tuile en fonction de l'orientation et de
@@ -89,26 +90,16 @@ typedef struct placed_tile {
 char tile_get_family_face(tile_t* tile, tile_orientation_t orientation,
                           tile_orientation_t connexion_face);
 
-return_code_t placed_tile_create(
-  placed_tile_t* placed_tile,
-  tile_t* parent,
-  tile_orientation_t orientation
-);
-void placed_tile_destroy(placed_tile_t*);
+return_code_t placed_tile_create(placed_tile_t* placed_tile, tile_t* parent,
+                                 tile_orientation_t orientation);
+void          placed_tile_destroy(placed_tile_t*);
 
+tile_orientation_t tile_orientation_invert(tile_orientation_t orientation);
 
-tile_orientation_t tile_orientation_invert(
-  tile_orientation_t orientation
-);
+tile_part_type_t tile_orientation_face(placed_tile_t*     placed_tile,
+                                       tile_orientation_t orientation);
 
-tile_part_type_t tile_orientation_face(
-  placed_tile_t* placed_tile,
-  tile_orientation_t orientation
-);
-
-placed_tile_group_t* tile_orientation_group(
-  placed_tile_t* placed_tile,
-  tile_orientation_t orientation
-);
+placed_tile_group_t* tile_orientation_group(placed_tile_t*     placed_tile,
+                                            tile_orientation_t orientation);
 
 #endif
