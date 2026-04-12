5f31a4011cf51db2232ad5fec0ad15de5a083e58
Matthieu Pignolet <matthieu@puffer.fish>
2026-04-12 18:37:58 +0200

    feat(libcarcassonne): add propagation for all face tiles (for fields for example)

--- lib/libcarcassonne/tile.c
+++ lib/libcarcassonne/tile.c
@@ -8,8 +8,8 @@ tile_part_type_t tile_get_family_face(tile_t*            tile,
                                       tile_orientation_t connexion_face) {
   if (tile == NULL) return 0;
 
-  int        index     = (connexion_face + orientation) % 4;
-  static int values[4] = {1, 5, 7, 3};
+  int              index     = (connexion_face + orientation) % 4;
+  static const int values[4] = {1, 5, 7, 3};
 
   return tile->parts[values[index]];
 }
@@ -18,16 +18,23 @@ tile_orientation_t tile_orientation_invert(tile_orientation_t orientation) {
   return (orientation + 2) % 4;
 }
 
-placed_tile_group_t* tile_orientation_group(placed_tile_t*     tile,
-                                            tile_orientation_t connexion_face) {
-  if (tile == NULL) return 0;
+return_code_t tile_get_face(placed_face_groups_t* ret, placed_tile_t* tile,
+                            tile_orientation_t connexion_face) {
+  if (tile == NULL) return ERROR;
 
-  int        index     = (connexion_face + tile->orientation) % 4;
-  static int values[4] = {1, 5, 7, 3};
+  int index = (connexion_face + tile->orientation) % 4;
 
-  unsigned int groupe = tile->parent->parts_groups[values[index]];
+  static const int values[4][3] = {{0, 1, 2}, {2, 5, 8}, {8, 7, 6}, {6, 3, 0}};
 
-  return tile->groups[groupe];
+  tile_part_group_t groups[3] = {tile->parent->parts_groups[values[index][0]],
+                                 tile->parent->parts_groups[values[index][1]],
+                                 tile->parent->parts_groups[values[index][2]]};
+
+  ret->face[0] = tile->groups[groups[0]];
+  ret->face[1] = tile->groups[groups[1]];
+  ret->face[2] = tile->groups[groups[2]];
+
+  return SUCCESS;
 }
 
 return_code_t placed_tile_create(placed_tile_t* placed_tile, tile_t* parent,
