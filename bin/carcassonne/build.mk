4ce2eddc20e281501736599e2411d2df5d170d36
Matthieu Pignolet <matthieu@puffer.fish>
2026-04-13 21:53:36 +0200

    feat(build): fix linking dynamic for mingw and other cross compiled targets

--- bin/carcassonne/build.mk
+++ bin/carcassonne/build.mk
@@ -1,8 +1,8 @@
 NAME := carcassonne
 PROG = $(BINS_DIR)/$(NAME)
 
-LFLAGS += `pkg-config sdl3 --libs`
-CFLAGS += `pkg-config sdl3 --cflags`
+LFLAGS += $(shell pkg-config --personality=$(TARGET) sdl3 --libs)
+CFLAGS += $(shell pkg-config --personality=$(TARGET) sdl3 --cflags)
 LLIBS  += carcassonne
 
 include $(BUILD_DIR)/binary.mk
