54587cc90d8ec19e94a8980da497cbf17348f5d0
Matthieu Pignolet <matthieu@puffer.fish>
2026-04-14 12:11:28 +0200

    feat: ajout de sdl3-image et l'utiliser cc. salomé

---
+++ bin/sdl/build.mk
@@ -0,0 +1,9 @@
+NAME := sdl
+PROG = $(BINS_DIR)/$(NAME)
+
+LFLAGS += $(shell pkg-config --personality=$(TARGET) sdl3 --libs)
+CFLAGS += $(shell pkg-config --personality=$(TARGET) sdl3 --cflags)
+LFLAGS += $(shell pkg-config --personality=$(TARGET) sdl3-image --libs)
+CFLAGS += $(shell pkg-config --personality=$(TARGET) sdl3-image --cflags)
+
+include $(BUILD_DIR)/binary.mk
