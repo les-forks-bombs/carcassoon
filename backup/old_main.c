05c5227945c3edea680270f7d627b958287c7888
Salomé <salome.poirson@telecomnancy.eu>
2026-04-16 13:24:16 +0200

    feat: initialization of the window with the new SDL3 standards

--- backup/old_main.c
+++ backup/old_main.c
@@ -1,6 +1,4 @@
-#define SDL_MAIN_USE_CALLBACKS 1
 #include <SDL3/SDL.h>
-#include <SDL3/SDL_main.h>
 #include <SDL3_image/SDL_image.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -37,8 +35,6 @@ SDL_AppResult SDL_AppIterate(void *appstate)
     while ((now - as->last_step) >= STEP_RATE_IN_MILLISECONDS) {
         as->last_step += STEP_RATE_IN_MILLISECONDS;
     }
-    SDL_SetRenderDrawColor(as->renderer, 0, 0, 0, 255);
-    SDL_RenderClear(as->renderer);
     SDL_RenderPresent(as->renderer);
     return SDL_APP_CONTINUE;
 }
@@ -59,8 +55,6 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
     if (!SDL_CreateWindowAndRenderer("Test fenêtre Carcassonne", WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_RESIZABLE, &as->window, &as->renderer)) {
         return SDL_APP_FAILURE;
     }
-
-    as->last_step = SDL_GetTicks();
     return SDL_APP_CONTINUE;
 }
 
