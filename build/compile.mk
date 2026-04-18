4b6460145f1f231112d2809827fa089cc66c2f88
Matthieu Pignolet <matthieu@puffer.fish>
2026-04-18 17:18:29 +0200

    feat: avoid paths issues & rework clean responsability

--- build/compile.mk
+++ build/compile.mk
@@ -1,14 +1,19 @@
-OBJ_DIR := $(OBJ_DIR)$(subst $(MAKE_DIR),,$(CURDIR))
+L_OBJ_DIR := $(OBJ_DIR)$(subst $(MAKE_DIR),,$(CURDIR))
 
 SRCS := $(wildcard *.c)
-OBJS := $(patsubst %.c, $(OBJ_DIR)/%.o, $(SRCS))
+OBJS := $(patsubst %.c, $(L_OBJ_DIR)/%.o, $(SRCS))
 
 # Build .o files from .c files
-$(OBJS): $(OBJ_DIR)/%.o: %.c
-	@mkdir -p $(OBJ_DIR)
+$(OBJS): $(L_OBJ_DIR)/%.o: %.c
+	@mkdir -p $(L_OBJ_DIR)
 	@$(CC) $(CFLAGS) -MMD -MP -c $< -o $@
 	@echo "    CC    $<"
 -include $(OBJS:.o=.d)
+
+clean::
+	@$(RM) -f $(OBJS)
+	@$(RM) -f $(OBJS:.o=.d)
+	@echo "    RM    $(notdir $(OBJS)) $(notdir $(OBJS:.o=.d))"
 
 build:: $(OBJS)
 test:: $(OBJS)
