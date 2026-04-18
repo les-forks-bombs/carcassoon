668a769075933f1bf90ead7e57bfc2f950d07c08
Matthieu Pignolet <matthieu@puffer.fish>
2026-04-18 17:32:43 +0200

    feat: move `runtime_config_t` to `libcarcassonne`

---
+++ lib/libcarcassonne/options.h
@@ -0,0 +1,24 @@
+#ifndef H_LIBCARCASSONNE_OPTIONS
+#define H_LIBCARCASSONNE_OPTIONS
+
+#include <getopt.h>
+#include <unistd.h>
+
+typedef enum runtime_mode {
+  CARCASSONNE_MODE_SDL = 0,
+  CARCASSONNE_MODE_CLI = 1,
+} options_mode_t;
+
+typedef struct runtime_config {
+  options_mode_t mode;
+  unsigned int   players;
+  unsigned int   seed;
+  unsigned int   ai;
+  unsigned int   max_turns;
+} options_t;
+
+options_t parse_options(int argc, char* argv[]);
+
+char* validate_options(options_t*);
+
+#endif
