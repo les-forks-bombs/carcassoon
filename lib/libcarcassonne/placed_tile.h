ab7986163ce76bddd4b4a7c9321165f955e9ddca
Matthieu Pignolet <matthieu@puffer.fish>
2026-04-19 15:05:07 +0200

    feat: add reversal, aggregation function and `weak_child` list

--- lib/libcarcassonne/placed_tile.h
+++ lib/libcarcassonne/placed_tile.h
@@ -8,7 +8,43 @@ typedef struct placed_tile_group {
   /// @brief Référence vers l'éventuel meeple placé dans le groupe
   meeple_t* meeple;
 
+  /**
+   * @brief Elements du L/C tree
+   */
   struct placed_tile_group *right, *left, *parent;
+
+  /**
+   * @brief Tableau des enfants au total
+   * Utilisé pour le parcours "simple" depuis la racine
+   */
+  struct placed_tile_group* weak_childs[3 * 4];
+  /**
+   * @brief Nombre d'enfants dans "weak_childs"
+   */
+  unsigned int weak_childs_count;
+
+  /**
+   * @brief
+   *
+   */
+  bool flipped_delta;
+
+  /**
+   * @brief Le nombre de faces a remplir pour que le groupe soit completé
+   * @remark Si ==0, alors le groupe est complété
+   */
+  unsigned int groupe_open_slots;
+
+  /**
+   * @brief Le nombre de tiles qui font partie du groupe
+   */
+  unsigned int groupe_taille;
+
+  /**
+   * @brief Le nombre de faces a remplir pour que le groupe soit completé
+   * @remark Si ==0, alors le groupe est complété
+   */
+  unsigned int local_open_slots;
 } placed_tile_group_t;
 
 typedef struct placed_face_groups {
@@ -40,3 +76,12 @@ void placed_tile_group_cut(placed_tile_group_t* node);
 void placed_tile_group_link(placed_tile_group_t* a, placed_tile_group_t* b);
 void placed_tile_group_splay(placed_tile_group_t* node);
 bool placed_tile_group_splay_is_root(placed_tile_group_t* node);
+
+void placed_tile_group_weak_childs_add(placed_tile_group_t* node,
+                                       placed_tile_group_t* el);
+void placed_tile_group_weak_childs_rem(placed_tile_group_t* node,
+                                       placed_tile_group_t* el);
+void placed_tile_group_push_down(placed_tile_group_t* node);
+void placed_tile_group_makeroot(placed_tile_group_t* node);
+
+bool placed_tile_group_complete(placed_tile_group_t* group);
