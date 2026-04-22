4787ea4b066a7206978e689d2f43a506bf5b43f9
Matthieu Pignolet <matthieu@puffer.fish>
2026-04-22 06:47:36 +0000

    fix: activer le vsync pour éviter de **tuer** le cpu & gpu

--- lib/sdl/main.c
+++ lib/sdl/main.c
@@ -1,3 +1,5 @@
+#include <SDL3/SDL_render.h>
+#include <stdbool.h>
 #define SDL_MAIN_USE_CALLBACKS 1
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
@@ -130,6 +132,8 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
                                    &as->renderer)) {
     return SDL_APP_FAILURE;
   }
+
+  SDL_SetRenderVSync(as->renderer, true);
 
   if (!TTF_Init()) {
     return SDL_APP_FAILURE;
