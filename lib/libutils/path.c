164601ab7c418bd55987dc31cc94a7dd9a643a19
Matthieu Pignolet <matthieu@puffer.fish>
2026-04-22 06:42:33 +0000

    feat: add `libutils` functions as a cross-platform abstraction to the executable dir

---
+++ lib/libutils/path.c
@@ -0,0 +1,60 @@
+
+#ifndef _WIN32
+#define _POSIX_C_SOURCE 200809L
+#endif
+
+#include <libutils/path.h>
+#include <string.h>
+
+#include "libcarcassonne/consts.h"
+
+#ifdef _WIN32
+#include <shlwapi.h>
+#include <stdio.h>
+#include <tchar.h>
+#include <windows.h>
+#else
+#include <libgen.h>
+#include <linux/limits.h>
+#include <stdio.h>
+#include <unistd.h>
+#endif
+
+return_code_t current_executable_path(char path[LIBUTILS_PATH_BUF]) {
+  ssize_t len;
+#ifdef _WIN32
+  len = GetModuleFileNameA(NULL, path, MAX_PATH);
+  if (len == 0) {
+    printf("GetModuleFileName failed. Error: %lu\n", GetLastError());
+    return ERROR;
+  } else if (len == MAX_PATH) {
+    printf("Path truncated (buffer too small).\n");
+    return ERROR;
+  }
+#else
+
+  len = readlink("/proc/self/exe", path, LIBUTILS_PATH_BUF - 1);
+  if (len == -1) {
+    perror("readlink failed");
+    return ERROR;
+  }
+  path[len] = '\0';
+#endif
+
+  return SUCCESS;
+}
+
+return_code_t current_executable_dir(char ret[LIBUTILS_PATH_BUF]) {
+  char path[LIBUTILS_PATH_BUF];
+  if (current_executable_path(path) != SUCCESS) return ERROR;
+  char* out = path;
+
+#ifdef _WIN32
+  PathRemoveFileSpecA(out);
+#else
+  out = dirname(path);
+#endif
+  strcpy(ret, out);
+
+  return SUCCESS;
+}
