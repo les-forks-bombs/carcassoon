8e402e1ce8282789c6e5655e1d0e759944636a20
default <matthieu@eu.mpgn.dev>
2026-04-28 09:22:58 +0000

    feat: line and clang-tidy config

--- lib/libcarcassonne/player.h
+++ lib/libcarcassonne/player.h
@@ -11,8 +11,8 @@ enum player_type {
 
 typedef enum player_type player_type_t;
 struct player {
-  player_type_t          player_type;
-  int                    score;
+  player_type_t         player_type;
+  int                   score;
   meeple_count_vector_t meeples_count;
   meeple_vector_t       meeples;
 };
