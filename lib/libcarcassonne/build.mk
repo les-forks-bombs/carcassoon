8765e9ac254d9a3ab735e695651a282a571b6700
Matthieu Pignolet <matthieu@puffer.fish>
2026-04-29 17:08:01 +0000

    Squashed commit of the following:
    commit 757ec87428c6ce00b64f9d1332fc04198837eda3
    Author: default <matthieu@eu.mpgn.dev>
    Date:   Wed Apr 29 17:05:51 2026 +0000
    
        fix: wrong dependency for the coverage.xml job
    
    commit 3e77af83b09bdd6cc069516404c21d5382943a59
    Author: default <matthieu@eu.mpgn.dev>
    Date:   Wed Apr 29 17:02:17 2026 +0000
    
        fix: use `/objs/` due to a mass replace mistake
    
    commit c42526763ad371b0aa8809a7639f61b64ec3e3a6
    Author: default <matthieu@eu.mpgn.dev>
    Date:   Wed Apr 29 16:59:40 2026 +0000
    
        fix: use unique types for OBJS and SRCS and use them to isolate files, and reorder build definition includes
    
    commit 3d548c94f92007ff68fb2d5f3e81a9b8ba0ade46
    Author: default <matthieu@eu.mpgn.dev>
    Date:   Wed Apr 29 16:47:55 2026 +0000
    
        fix: fix coverage xml file generation
    
    commit d48cde96fa47087da4008e693f20882635cf0dd6
    Author: default <matthieu@eu.mpgn.dev>
    Date:   Wed Apr 29 16:45:25 2026 +0000
    
        fix: use $^ instead of $? when building & linking
    
    commit 09ceb7ad55157df234edcf671eac28145f0e4811
    Author: default <matthieu@eu.mpgn.dev>
    Date:   Wed Apr 29 16:35:10 2026 +0000
    
        chore: do not silence make in ci
    
    commit d0db9ded1f36da64f2ace6c85e7a7119cb33ea37
    Author: default <matthieu@eu.mpgn.dev>
    Date:   Wed Apr 29 16:33:02 2026 +0000
    
        chore: verbose logging
    
    commit 3d953e6f0e28c20a99f4a2b99ce508faae9b8724
    Author: default <matthieu@eu.mpgn.dev>
    Date:   Wed Apr 29 15:44:37 2026 +0000
    
        fix: upload test files
    
    commit d89dc7a3bdb6487e55c7b673da717675018a0cc6
    Author: default <matthieu@eu.mpgn.dev>
    Date:   Wed Apr 29 15:42:02 2026 +0000
    
        fix: make `bear`create the needed build_commands.json parent directory
    
    commit b7763098434c65af00e8df8761a64680c80b7f4f
    Author: default <matthieu@eu.mpgn.dev>
    Date:   Wed Apr 29 15:39:08 2026 +0000
    
        feat(build): avoid using make recursively

--- lib/libcarcassonne/build.mk
+++ lib/libcarcassonne/build.mk
@@ -1,4 +1,20 @@
-NAME := libcarcassonne
+DIR_STACK := $(DIR) $(DIR_STACK)
+DIR := $(DIR)/libcarcassonne
 
-include $(BUILD_DIR)/library.mk
-include $(BUILD_DIR)/testable.mk
+LIBCARCASSONNE_SRCS := $(wildcard $(DIR)/*.c)
+LIBCARCASSONNE_OBJS := $(patsubst $(PWD)/lib/%.c, $(OUT)/objs/%.o, $(LIBCARCASSONNE_SRCS))
+
+CLEAN += $(LIBCARCASSONNE_OBJS) $(LIBCARCASSONNE_OBJS:.o=.d)
+-include $(LIBCARCASSONNE_OBJS:.o=.d)
+
+$(OUT)/libcarcassonne.a: $(LIBCARCASSONNE_OBJS)
+	@mkdir -p $(dir $@)
+	@ar rcs $@ $^
+	$(info $(TAB)AR $@)
+
+CLEAN += $(OUT)/libcarcassonne.a
+
+include $(DIR)/tests/build.mk
+
+DIR := $(firstword $(DIR_STACK))
+DIR_STACK := $(wordlist 2, $(words $(DIR_STACK)), $(DIR_STACK))
