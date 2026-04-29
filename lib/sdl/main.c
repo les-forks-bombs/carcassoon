0910e0899f3586b28848925b89ff448eae15fe4e
Matthieu Pignolet <matthieu@puffer.fish>
2026-04-29 19:35:28 +0000

    feat: use path resolver for sdl assets

--- lib/sdl/main.c
+++ lib/sdl/main.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 
 #include "libutils/path.h"
+#include "sdl/resolver.h"
 #define SDL_MAIN_USE_CALLBACKS 1
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
@@ -139,6 +140,8 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
   (void)appstate;
   (void)argc;
   (void)argv;
+
+  create_path_resolver(&resolver);
 
   if (!SDL_Init(SDL_INIT_VIDEO)) return SDL_APP_FAILURE;
 
