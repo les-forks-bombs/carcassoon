a059a12248e688743826d410605746a59811a395
default <matthieu@eu.mpgn.dev>
2026-04-29 17:08:01 +0000

    Squashed commit of the following:
    commit 757ec87428c6ce00b64f9d1332fc04198837eda3
    Author: default <matthieu@eu.mpgn.dev>
    Date:   Wed Apr 29 17:05:51 2026 +0000
    
        fix: wrong dependency for the coverage.xml job
    
    commit 3e77af83b09bdd6cc069516404c21d5382943a59
    Author: default <matthieu@eu.mpgn.dev>
    Date:   Wed Apr 29 17:02:17 2026 +0000
    
        fix: use `/objs/` due to a mass replace mistake
    
    commit c42526763ad371b0aa8809a7639f61b64ec3e3a6
    Author: default <matthieu@eu.mpgn.dev>
    Date:   Wed Apr 29 16:59:40 2026 +0000
    
        fix: use unique types for OBJS and SRCS and use them to isolate files, and reorder build definition includes
    
    commit 3d548c94f92007ff68fb2d5f3e81a9b8ba0ade46
    Author: default <matthieu@eu.mpgn.dev>
    Date:   Wed Apr 29 16:47:55 2026 +0000
    
        fix: fix coverage xml file generation
    
    commit d48cde96fa47087da4008e693f20882635cf0dd6
    Author: default <matthieu@eu.mpgn.dev>
    Date:   Wed Apr 29 16:45:25 2026 +0000
    
        fix: use $^ instead of $? when building & linking
    
    commit 09ceb7ad55157df234edcf671eac28145f0e4811
    Author: default <matthieu@eu.mpgn.dev>
    Date:   Wed Apr 29 16:35:10 2026 +0000
    
        chore: do not silence make in ci
    
    commit d0db9ded1f36da64f2ace6c85e7a7119cb33ea37
    Author: default <matthieu@eu.mpgn.dev>
    Date:   Wed Apr 29 16:33:02 2026 +0000
    
        chore: verbose logging
    
    commit 3d953e6f0e28c20a99f4a2b99ce508faae9b8724
    Author: default <matthieu@eu.mpgn.dev>
    Date:   Wed Apr 29 15:44:37 2026 +0000
    
        fix: upload test files
    
    commit d89dc7a3bdb6487e55c7b673da717675018a0cc6
    Author: default <matthieu@eu.mpgn.dev>
    Date:   Wed Apr 29 15:42:02 2026 +0000
    
        fix: make `bear`create the needed build_commands.json parent directory
    
    commit b7763098434c65af00e8df8761a64680c80b7f4f
    Author: default <matthieu@eu.mpgn.dev>
    Date:   Wed Apr 29 15:39:08 2026 +0000
    
        feat(build): avoid using make recursively

--- lib/carcassonne/build.mk
+++ lib/carcassonne/build.mk
@@ -1,13 +1,23 @@
-NAME := carcassonne
-PROG = $(BINS_DIR)/$(NAME)
+DIR_STACK := $(DIR) $(DIR_STACK)
+DIR := $(DIR)/carcassonne
 
-LFLAGS += $(shell pkg-config --personality=$(TARGET) sdl3 --libs)
-CFLAGS += $(shell pkg-config --personality=$(TARGET) sdl3 --cflags)
-LFLAGS += $(shell pkg-config --personality=$(TARGET) sdl3-image --libs)
-CFLAGS += $(shell pkg-config --personality=$(TARGET) sdl3-image --cflags)
-LFLAGS += $(shell pkg-config --personality=$(TARGET) sdl3-ttf --libs)
-CFLAGS += $(shell pkg-config --personality=$(TARGET) sdl3-ttf --cflags)
+CARCASSONNE_SRCS := $(wildcard $(DIR)/*.c)
+CARCASSONNE_OBJS := $(patsubst $(PWD)/lib/%.c, $(OUT)/objs/%.o, $(CARCASSONNE_SRCS))
 
-LLIBS  += ai carcassonne utils
+CLEAN += $(CARCASSONNE_OBJS) $(CARCASSONNE_OBJS:.o=.d)
+-include $(CARCASSONNE_OBJS:.o=.d)
 
-include $(BUILD_DIR)/binary.mk
+$(OUT)/bin/carcassonne$(EXT): $(CARCASSONNE_OBJS) $(OUT)/libcarcassonne.a $(OUT)/libutils.a $(OUT)/libai.a
+	@mkdir -p $(dir $@)
+	@$(CC) $(CFLAGS) $(LFLAGS) -o $@ $^ -lai -lcarcassonne -lutils
+	@case "$(TARGET)" in \
+	        (x86_64-w64-mingw64|x86_64-w64-mingw32) \
+	            $(UTIL_DIR)/copy_dlls.sh $@; \
+	            ;; \
+	esac
+	$(info $(TAB)LD $@)
+
+CLEAN += $(OUT)/bin/carcassonne$(EXT)
+
+DIR := $(firstword $(DIR_STACK))
+DIR_STACK := $(wordlist 2, $(words $(DIR_STACK)), $(DIR_STACK))
--- lib/libutils/tests/build.mk
+++ lib/libutils/tests/build.mk
@@ -1,4 +1,25 @@
-NAME := utils_test
-LLIBS = utils
+DIR_STACK := $(DIR) $(DIR_STACK)
+DIR := $(DIR)/tests
 
-include $(BUILD_DIR)/test.mk
+LIBUTILS_TEST_SRCS := $(wildcard $(DIR)/*.c)
+LIBUTILS_TEST_OBJS := $(patsubst $(PWD)/lib/%.c, $(OUT)/objs/%.o, $(LIBUTILS_TEST_SRCS))
+
+CLEAN += $(LIBUTILS_TEST_OBJS) $(LIBUTILS_TEST_OBJS:.o=.d)
+-include $(LIBUTILS_TEST_OBJS:.o=.d)
+
+$(OUT)/bin/libutils_test$(EXT): $(LIBUTILS_TEST_OBJS) \
+    $(OUT)/libutils.a 
+	@mkdir -p $(dir $@)
+	@$(CC) $(CFLAGS) $(LFLAGS) -o $@ $^ -lcmocka
+	@case "$(TARGET)" in \
+	        (x86_64-w64-mingw64|x86_64-w64-mingw32) \
+	            $(UTIL_DIR)/copy_dlls.sh $@; \
+	            ;; \
+	esac
+	$(info $(TAB)LD $@)
+
+CLEAN += $(OUT)/bin/libutils_test$(EXT)
+TESTS += $(OUT)/bin/libutils_test$(EXT)
+
+DIR := $(firstword $(DIR_STACK))
+DIR_STACK := $(wordlist 2, $(words $(DIR_STACK)), $(DIR_STACK))
