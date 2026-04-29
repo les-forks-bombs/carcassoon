0b564e648fb235517e52c4d1c7679d9f6af8ab9f
Matthieu Pignolet <matthieu@puffer.fish>
2026-04-29 17:08:01 +0000

    Squashed commit of the following:
    commit 3db37f086b05bf74ee39444a9a60d331de426adb
    Author: default <matthieu@eu.mpgn.dev>
    Date:   Wed Apr 29 17:05:51 2026 +0000
    
        fix: wrong dependency for the coverage.xml job
    
    commit 14acc3f8dc57b8a964ab5153e9718af7ccd461be
    Author: default <matthieu@eu.mpgn.dev>
    Date:   Wed Apr 29 17:02:17 2026 +0000
    
        fix: use `/objs/` due to a mass replace mistake
    
    commit 6103444461ca0f40e246ef313c56f692053fe9cf
    Author: default <matthieu@eu.mpgn.dev>
    Date:   Wed Apr 29 16:59:40 2026 +0000
    
        fix: use unique types for OBJS and SRCS and use them to isolate files, and reorder build definition includes
    
    commit f4469aba1298d95f5ec329c550019481cc47bb77
    Author: default <matthieu@eu.mpgn.dev>
    Date:   Wed Apr 29 16:47:55 2026 +0000
    
        fix: fix coverage xml file generation
    
    commit 3decd120f773e041663a93a95c7ce53b6b204f16
    Author: default <matthieu@eu.mpgn.dev>
    Date:   Wed Apr 29 16:45:25 2026 +0000
    
        fix: use $^ instead of $? when building & linking
    
    commit 48f5369980b139a8db75ecce0fb3e1ddfe414738
    Author: default <matthieu@eu.mpgn.dev>
    Date:   Wed Apr 29 16:35:10 2026 +0000
    
        chore: do not silence make in ci
    
    commit f1ae7c49ca3631cd70a6c5aa104923aefa93d35f
    Author: default <matthieu@eu.mpgn.dev>
    Date:   Wed Apr 29 16:33:02 2026 +0000
    
        chore: verbose logging
    
    commit c5f81e9a424ef2fec238dc90c8438ed1e31d9fba
    Author: default <matthieu@eu.mpgn.dev>
    Date:   Wed Apr 29 15:44:37 2026 +0000
    
        fix: upload test files
    
    commit ab19368ee53656ffd59bbd4e90c2f664b569ec58
    Author: default <matthieu@eu.mpgn.dev>
    Date:   Wed Apr 29 15:42:02 2026 +0000
    
        fix: make `bear`create the needed build_commands.json parent directory
    
    commit edce41f4e7918d30004e7f6fdbc9d668f97d800d
    Author: default <matthieu@eu.mpgn.dev>
    Date:   Wed Apr 29 15:39:08 2026 +0000
    
        feat(build): avoid using make recursively

--- lib/libai/build.mk
+++ lib/libai/build.mk
@@ -1,4 +1,19 @@
-NAME := libai
+DIR_STACK := $(DIR) $(DIR_STACK)
+DIR := $(DIR)/libai
 
-include $(BUILD_DIR)/library.mk
-include $(BUILD_DIR)/testable.mk
+LIBAI_SRCS := $(wildcard $(DIR)/*.c)
+LIBAI_OBJS := $(patsubst $(PWD)/lib/%.c, $(OUT)/objs/%.o, $(LIBAI_SRCS))
+
+CLEAN += $(LIBAI_OBJS) $(LIBAI_OBJS:.o=.d)
+-include $(LIBAI_OBJS:.o=.d)
+
+$(OUT)/libai.a: $(LIBAI_OBJS)
+	@mkdir -p $(dir $@)
+	@ar rcs $@ $^
+	$(info $(TAB)AR $@)
+CLEAN += $(OUT)/libai.a
+
+include $(DIR)/tests/build.mk
+
+DIR := $(firstword $(DIR_STACK))
+DIR_STACK := $(wordlist 2, $(words $(DIR_STACK)), $(DIR_STACK))
