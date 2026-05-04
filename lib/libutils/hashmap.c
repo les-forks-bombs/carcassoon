816a58936d40fe2cd9fa0582ac9c4acd8ee895d2
Damien Jung <damien.jung@telecomnancy.eu>
2026-05-05 00:42:29 +0200

    test(libutils): Finishing get-set test for hashmap with Matthieu Pignolet

--- lib/libutils/hashmap.c
+++ lib/libutils/hashmap.c
@@ -1,3 +1,5 @@
+#include "hashmap.h"
+
 #include <libutils/hashmap.h>
 #include <stddef.h>
 #include <stdio.h>
@@ -27,14 +29,17 @@ void utils_hashmap_create(hashmap_t *map, size_t number_of_buckets) {
 
 void utils_hashmap_set(hashmap_t *map, const void *key, size_t key_size,
                        const void *data, size_t data_size) {
-  unsigned int    index    = hash(key, key_size);
-  hashmap_node_t *new_node = calloc(1, sizeof(hashmap_node_t));
-  new_node->key            = key;
-  new_node->value          = data;
+  unsigned int index = hash(key, key_size) % map->size;
+
+  hashmap_node_t new_node;
+  new_node.key = malloc(key_size);
+  memcpy(new_node.key, key, key_size);
+  new_node.value = malloc(data_size);
+  memcpy(new_node.value, data, data_size);
 
   list_t *list = ((list_t *)vector_nth(&map->buckets, index));
 
-  utils_list_prepend(list, data, data_size);
+  utils_list_prepend(list, &new_node, sizeof(hashmap_node_t));
 }
 
 void *utils_hashmap_get(const hashmap_t *map, const void *key,
@@ -43,9 +48,13 @@ void *utils_hashmap_get(const hashmap_t *map, const void *key,
 
   list_t *list = ((list_t *)vector_nth(&map->buckets, index));
 
+  if (list->head == NULL && list->tail == NULL) {
+    return NULL;
+  }
+
   list_node_t    *list_node = list->head;
   hashmap_node_t *current   = ((hashmap_node_t *)list_node->value);
-  while (list_node != NULL && strcmp(current->key, key) != 0) {
+  while (list_node != NULL && memcmp(current->key, key, key_size) != 0) {
     list_node = list_node->next;
     current   = ((hashmap_node_t *)list_node->value);
   }
@@ -61,11 +70,12 @@ void utils_hashmap_free(hashmap_t *map) {
     list_t *list = ((list_t *)vector_nth(&map->buckets, i));
 
     list_node_t *list_node = list->head;
-    list_node_t *next;
     while (list_node != NULL) {
-      next = list_node->next;
-      free(((hashmap_node_t *)list_node->value));
-      list_node = next;
+      hashmap_node_t *node = (hashmap_node_t *)list_node->value;
+      free(node->value);
+      free(node->key);
+
+      list_node = list_node->next;
     }
 
     utils_list_free(list);
