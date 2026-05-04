63d04befd97adcff766f17dcb7fafc1d1e587067
Matthieu Pignolet <matthieu@puffer.fish>
2026-05-04 19:51:34 +0200

    feat(libutils): fix bucket creation logic

--- lib/libutils/hashmap.c
+++ lib/libutils/hashmap.c
@@ -12,20 +12,21 @@ void utils_hashmap_create(hashmap_t *map, size_t number_of_buckets) {
   if (map == NULL || number_of_buckets < 1) {
     return;
   }
+  list_t list;
+  list.head = NULL;
+  list.tail = NULL;
+  list.size = 0;
 
   vector_alloc(&map->buckets, number_of_buckets);
   for (size_t i = 0; i < number_of_buckets; i++) {
-    list_t *list = ((list_t *)vector_nth(&map->buckets, i));
-    list->head   = NULL;
-    list->tail   = NULL;
-    list->size   = 0;
+    vector_append(&map->buckets, &list);
   }
   map->element_count = 0;
   map->size          = number_of_buckets;
 }
 
 void utils_hashmap_set(hashmap_t *map, const void *key, size_t key_size,
-                          const void *data, size_t data_size) {
+                       const void *data, size_t data_size) {
   unsigned int    index    = hash(key, key_size);
   hashmap_node_t *new_node = calloc(1, sizeof(hashmap_node_t));
   new_node->key            = key;
@@ -36,7 +37,8 @@ void utils_hashmap_set(hashmap_t *map, const void *key, size_t key_size,
   utils_list_prepend(list, data, data_size);
 }
 
-void *utils_hashmap_get(const hashmap_t *map, const void *key, size_t key_size) {
+void *utils_hashmap_get(const hashmap_t *map, const void *key,
+                        size_t key_size) {
   unsigned int index = hash(key, key_size) % map->size;
 
   list_t *list = ((list_t *)vector_nth(&map->buckets, index));
