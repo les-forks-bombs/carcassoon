76abdbcdacb4957ea9aa81620e23aea832aeca63
Matthieu Pignolet <matthieu@puffer.fish>
2026-05-04 20:02:38 +0200

    fix: memory initialization for map_t

--- lib/libutils/tests/hashmap.c
+++ lib/libutils/tests/hashmap.c
@@ -1,8 +1,10 @@
 #include <libutils/tests/tests.h>
 #include <libutils/hashmap.h>
+#include <string.h>
 
 void utils_hashmap_create_test(void** state){
     map_t map;
+    memset(&map, 0, sizeof(map));
     hashmap_create(&map, 5);
 
     hashmap_free(&map);
