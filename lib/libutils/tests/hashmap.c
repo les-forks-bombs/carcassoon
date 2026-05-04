352966697643403a9169352106f74408c70eadbb
Damien Jung <damien.jung@telecomnancy.eu>
2026-05-04 23:11:48 +0200

    test(libutils): Adding get-set test for hashmap

--- lib/libutils/tests/hashmap.c
+++ lib/libutils/tests/hashmap.c
@@ -1,11 +1,23 @@
-#include <libutils/tests/tests.h>
+#include <cmocka.h>
 #include <libutils/hashmap.h>
+#include <libutils/tests/tests.h>
 #include <string.h>
 
-void utils_hashmap_create_test(void** state){
-    map_t map;
-    memset(&map, 0, sizeof(map));
-    hashmap_create(&map, 5);
+void utils_hashmap_create_test(void** state) {
+  map_t map;
+  memset(&map, 0, sizeof(map));
+  hashmap_create(&map, 5);
 
-    hashmap_free(&map);
+  hashmap_free(&map);
+}
+
+void utils_hashmap_get_set_test(void** state) {
+  map_t map;
+  memset(&map, 0, sizeof(map));
+  hashmap_create(&map, 5);
+
+  char key1[20] = "hehe";
+  assert_null(hashmap_get(&map, &key1));
+
+  hashmap_free(&map);
 }
