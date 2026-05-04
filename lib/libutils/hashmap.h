15bc9f3049341a07521394e13be9547ccd7226dd
Damien Jung <damien.jung@telecomnancy.eu>
2026-05-04 23:12:18 +0200

    fix(libutils): Fixing seg fault while getting in empty hashmap

--- lib/libutils/hashmap.h
+++ lib/libutils/hashmap.h
@@ -22,19 +22,19 @@ typedef struct hashmap {
   unsigned int    element_count;
 } hashmap_t;
 
-#define HashMap(key_type, value_type)           \
-  union {                   \
-    hashmap_t      meta;        \
-    key_type   *key_ghost;   \
-    value_type *value_ghost; \
+#define HashMap(key_type, value_type) \
+  union {                             \
+    hashmap_t   meta;                 \
+    key_type   *key_ghost;            \
+    value_type *value_ghost;          \
   }
 
 unsigned int hash(const void *key, size_t key_size);
 
 void utils_hashmap_create(hashmap_t *map, size_t number_of_buckets);
 
-void  utils_hashmap_set(hashmap_t *map, const void *key, size_t key_size, const void *data,
-                        size_t data_size);
+void  utils_hashmap_set(hashmap_t *map, const void *key, size_t key_size,
+                        const void *data, size_t data_size);
 void *utils_hashmap_get(const hashmap_t *map, const void *key, size_t key_size);
 void  utils_hashmap_free(hashmap_t *map);
 void  utils_hashmap_remove(hashmap_t *map, void *key, size_t key_size);
@@ -42,15 +42,15 @@ void  utils_hashmap_remove(hashmap_t *map, void *key, size_t key_size);
 #define hashmap_create(map, number_of_buckets) \
   utils_hashmap_create(&((map)->meta), number_of_buckets)
 
-#define hashmap_set(map, key, value)                                 \
-  utils_hashmap_set(&((map)->meta),                                  \
-                    (const void *)(1 ? (key) : (map)->key_ghost),    \
-                    sizeof(*(map)->key_ghost),                       \
+#define hashmap_set(map, key, value)                                  \
+  utils_hashmap_set(&((map)->meta),                                   \
+                    (const void *)(1 ? (key) : (map)->key_ghost),     \
+                    sizeof(*(map)->key_ghost),                        \
                     (const void *)(1 ? (value) : (map)->value_ghost), \
                     sizeof(*(map)->value_ghost))
 
 #define hashmap_get(map, key)                                       \
-  ((__typeof__((map)->value_ghost))utils_hashmap_get(              \
+  ((__typeof__((map)->value_ghost))utils_hashmap_get(               \
       &((map)->meta), (const void *)(1 ? (key) : (map)->key_ghost), \
       sizeof(*(map)->key_ghost)))
 
