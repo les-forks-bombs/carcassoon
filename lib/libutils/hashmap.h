c7df610d20e2b2d9d728001ee6277099cf6c87f4
Bastien VINCENT--DEKENS <bastien.vincent--dekens@telecomnancy.eu>
2026-05-22 09:54:15 +0200

    feat: correction of hashmap

--- lib/libutils/hashmap.h
+++ lib/libutils/hashmap.h
@@ -10,8 +10,9 @@
 #define LIBUTILS_HASHMAP_SEED 67
 
 typedef struct hashmap_node {
-  char *key;
-  char *value;
+  void *key;
+  void *value;
+  size_t key_size;
 } hashmap_node_t;
 
 typedef Vector(list_t) bucket_vector_t;
@@ -37,26 +38,18 @@ void  utils_hashmap_set(hashmap_t *map, const void *key, size_t key_size,
                         const void *data, size_t data_size);
 void *utils_hashmap_get(const hashmap_t *map, const void *key, size_t key_size);
 void  utils_hashmap_free(hashmap_t *map);
-void  utils_hashmap_remove(hashmap_t *map, void *key, size_t key_size);
+void  utils_hashmap_remove(hashmap_t *map, const void *key, size_t key_size);
 
 #define hashmap_create(map, number_of_buckets) \
   utils_hashmap_create(&((map)->meta), number_of_buckets)
 
-#define hashmap_set(map, key, value)                                  \
-  utils_hashmap_set(&((map)->meta),                                   \
-                    (const void *)(1 ? (key) : (map)->key_ghost),     \
-                    sizeof(*(map)->key_ghost),                        \
-                    (const void *)(1 ? (value) : (map)->value_ghost), \
-                    sizeof(*(map)->value_ghost))
+#define hashmap_set(map, key, key_size, value, value_size)              \
+  utils_hashmap_set(&((map)->meta), (key), (key_size), (value), (value_size))
 
-#define hashmap_get(map, key)                                       \
-  ((__typeof__((map)->value_ghost))utils_hashmap_get(               \
-      &((map)->meta), (const void *)(1 ? (key) : (map)->key_ghost), \
-      sizeof(*(map)->key_ghost)))
+#define hashmap_get(map, key, key_size)                                \
+  utils_hashmap_get(&((map)->meta), (key), (key_size))
 
-#define hashmap_remove(map, key)                                     \
-  utils_hashmap_remove(&((map)->meta),                               \
-                       (const void *)(1 ? (key) : (map)->key_ghost), \
-                       sizeof(*(map)->key_ghost))
+#define hashmap_remove(map, key, key_size)                              \
+  utils_hashmap_remove(&((map)->meta), (key), (key_size))
 
 #define hashmap_free(map) utils_hashmap_free(&((map)->meta))
