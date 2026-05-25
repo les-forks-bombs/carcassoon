1dc953dbe60567346dcaba5901cbcde540816572
Salome P <salome.poirson@gmail.com>
2026-05-25 21:37:57 +0000

    refactor(sdl): split main functions into separate files

---
+++ lib/sdl/load.c
@@ -0,0 +1,47 @@
+#include <dirent.h>
+#include <sdl/load.h>
+#include <stdio.h>
+#include "libutils/path.h"
+#include <sys/stat.h>
+
+void load_texture(AppState *state, char *name, char *path) {
+  SDL_Texture *texture = IMG_LoadTexture(state->renderer, path);
+
+  hashmap_set(&state->textures, name, strlen(name) + 1, &texture,
+              sizeof(SDL_Texture *));
+}
+
+void load_textures(AppState *appstate, char *directory, char *assets) {
+  DIR *dossier = opendir(directory);
+  if (dossier == NULL) {
+    perror("Erreur lors de l'ouverture du dossier");
+    return;
+  }
+
+  struct dirent *entree;
+  char           chemin_complet[LIBUTILS_PATH_BUF];
+
+  while ((entree = readdir(dossier)) != NULL) {
+    if (strcmp(entree->d_name, ".") == 0 || strcmp(entree->d_name, "..") == 0) {
+      continue;
+    }
+    snprintf(chemin_complet, sizeof(chemin_complet), "%s/%s", directory,
+             entree->d_name);
+
+    struct stat info_chemin;
+    if (stat(chemin_complet, &info_chemin) == 0) {
+      if (S_ISDIR(info_chemin.st_mode)) {
+        // printf("[DOSSIER] %s\n", chemin_complet);
+        load_textures(appstate, chemin_complet, assets);
+      } else if (S_ISREG(info_chemin.st_mode)) {
+        // printf("[FICHIER] %s\n", chemin_complet);
+
+        char *start = &chemin_complet[strlen(assets)];
+        // printf("loading texture %s at %s\n", start, chemin_complet);
+
+        load_texture(appstate, start, chemin_complet);
+      }
+    }
+  }
+  closedir(dossier);
+}
