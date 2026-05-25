2ccfd249f0167808919c81c21e6cf0a7cd2c3a3e
Salomé <salome.poirson@telecomnancy.eu>
2026-05-25 08:41:14 +0000

    feat(sdl): highlight the last placed tile

--- lib/sdl/map.h
+++ lib/sdl/map.h
@@ -6,6 +6,6 @@
 #include <libcarcassonne/forward.h>
 #include <sdl/meeple.h>
 
-void render_map(game_t *game, SDL_Renderer *renderer, camera_t *cam,SDL_Texture *test_tex);
+void render_map(game_t *game, SDL_Renderer *renderer, camera_t *cam,SDL_Texture *test_tex, placed_tile_t *current_tile);
 
 #endif
