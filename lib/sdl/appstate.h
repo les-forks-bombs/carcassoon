b4ddc3cb1a746459f5746876fa716e836fe91049
Salome P <salome.poirson@gmail.com>
2026-05-25 17:54:10 +0000

    feat(sdl): static colors

--- lib/sdl/appstate.h
+++ lib/sdl/appstate.h
@@ -1,23 +1,23 @@
 #ifndef APPSTATE_H
 #define APPSTATE_H
 
-#include <sdl/banner.h>
-#include <sdl/camera.h>
 #include <libcarcassonne/engine.h>
 #include <libutils/hashmap.h>
+#include <sdl/banner.h>
+#include <sdl/camera.h>
 
-typedef HashMap(char *, SDL_Texture*) textures_hashmap_t;
+typedef HashMap(char *, SDL_Texture *) textures_hashmap_t;
 
 typedef struct {
-  SDL_Window    *window;
-  SDL_Renderer  *renderer;
-  camera_t      *camera;
-  SDL_FRect      map_viewport;
-  Uint64         last_step;
-  text_object_t *text;
-  engine_t       engine;
-  SDL_Texture   *temp_tex;
-  placed_tile_t *current_tile;
+  SDL_Window        *window;
+  SDL_Renderer      *renderer;
+  camera_t          *camera;
+  SDL_FRect          map_viewport;
+  Uint64             last_step;
+  text_object_t     *text;
+  engine_t           engine;
+  SDL_Texture       *temp_tex;
+  placed_tile_t     *current_tile;
   textures_hashmap_t textures;
 
   banner_t **banners;
--- lib/sdl/map.h
+++ lib/sdl/map.h
@@ -2,10 +2,10 @@
 #define H_MAP
 
 #include <libcarcassonne/forward.h>
+#include <sdl/appstate.h>
 #include <sdl/camera.h>
 #include <sdl/consts.h>
 #include <sdl/meeple.h>
-#include <sdl/appstate.h>
 
 void render_map(AppState *as);
 
