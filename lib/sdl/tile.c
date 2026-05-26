2a265f97e8850fbda254d627b7eefddcddc8de98
Salomé <salome.poirson@telecomnancy.eu>
2026-05-26 14:45:40 +0000

    feat(sdl): tile preview and keyboard control

--- lib/sdl/tile.c
+++ lib/sdl/tile.c
@@ -1,3 +1,35 @@
 #include <sdl/tile.h>
+#include "libutils/vector.h"
+#include <stdio.h>
 
-void 
+void get_current_actions(AppState *as){
+    as->all_actions = engine_get_actions(&as->engine);
+}
+
+void put_first_action_in_appstate(AppState *as) {
+    as->current_action=vector_nth(&as->all_actions, 0);
+    as->is_waiting_for_tile=true;
+    as->action_index=0;
+}
+
+void pass_to_action(AppState *as, int increment) {
+    size_t total_actions = vector_size(&as->all_actions);
+
+    if (total_actions == 0) {
+        return;
+    }
+    int next_index = (int)as->action_index + increment;
+
+    if (next_index >= (int)total_actions) {
+        put_first_action_in_appstate(as);
+    } 
+    else if (next_index < 0) {
+        as->action_index = (int)total_actions - 1;
+        as->current_action = vector_nth(&as->all_actions, as->action_index);
+    } 
+    else {
+        as->action_index = (size_t)next_index;
+        as->current_action = vector_nth(&as->all_actions, as->action_index);
+    }
+    printf("action changée\n");
+}
