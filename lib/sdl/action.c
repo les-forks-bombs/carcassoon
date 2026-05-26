c579d70abb33b44509e6bd92e5114cd1699d635f
Salomé <salome.poirson@telecomnancy.eu>
2026-05-26 20:27:12 +0000

    feat(sdl): display possible meeples on the last tile

--- lib/sdl/action.c
+++ lib/sdl/action.c
@@ -3,7 +3,16 @@
 #include <stdio.h>
 
 void get_current_actions(AppState *as){
-    as->all_actions = engine_get_actions(&as->engine);
+    action_vector_t actions = engine_get_actions(&as->engine);
+
+    vector_free(&as->all_actions);
+
+    for (int i = 0; i < vector_size(&actions); i++) {
+        action_t *a = vector_nth(&actions, i);
+        if (a->type == LIBCARCASSONNE_ACTION_PLACE_TILE || a->order.place_meeple.tile!=NULL) {
+            vector_append(&as->all_actions, a);
+        }
+    }
 }
 
 void put_first_action_in_appstate(AppState *as) {
@@ -31,5 +40,5 @@ void pass_to_action(AppState *as, int increment) {
         as->action_index = (size_t)next_index;
         as->current_action = vector_nth(&as->all_actions, as->action_index);
     }
-    printf("action changée\n");
+    // printf("action changée\n");
 }
