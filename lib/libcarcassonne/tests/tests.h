839ee5e5ada5b2bdd6f9d69bf826a9bffafd3c42
Bastien VINCENT--DEKENS <bastien.vincent--dekens@telecomnancy.eu>
2026-05-27 14:41:19 +0200

    Feat: engine fix = units tests

--- lib/libcarcassonne/tests/tests.h
+++ lib/libcarcassonne/tests/tests.h
@@ -97,6 +97,71 @@ void placed_tile_field_fcfc_not_complete(void** state);
 void placed_tile_road_link_propagates_completeness(void** state);
 void placed_tile_road_l_shape_not_complete(void** state);
 
+/* Tests pour game_queries */
+void game_is_place_open_true_with_empty_neighbors(void** state);
+void game_is_place_open_false_when_all_neighbors_occupied(void** state);
+void game_is_space_available_true_for_empty(void** state);
+void game_is_space_available_false_when_occupied(void** state);
+void game_is_tile_placeable_null_game_returns_false(void** state);
+void game_is_tile_placeable_null_tile_returns_false(void** state);
+void game_get_current_player_returns_first_player(void** state);
+void game_get_current_player_advances_after_end_turn(void** state);
+
+/* Tests pour game_meeple */
+void game_place_meeple_success(void** state);
+void game_place_meeple_on_occupied_group_fails(void** state);
+void game_place_meeple_null_game_fails(void** state);
+void game_place_meeple_none_is_noop(void** state);
+void game_remove_meeple_success(void** state);
+void game_remove_meeple_empty_group_succeeds(void** state);
+void game_remove_meeple_null_game_fails(void** state);
+
+/* Tests pour game_turn */
+void game_end_player_turn_advances_player(void** state);
+void game_end_player_turn_at_last_returns_no_more_player(void** state);
+void game_end_round_resets_player_and_increments_turn(void** state);
+void game_end_round_before_last_player_fails(void** state);
+void is_game_finished_false_initially(void** state);
+void is_game_finished_false_with_no_turns_limit(void** state);
+void is_game_finished_true_when_turn_limit_reached(void** state);
+void game_end_player_turn_null_game_fails(void** state);
+void game_end_round_null_game_fails(void** state);
+
+/* Tests pour game_remove_tile */
+void game_remove_tile_success(void** state);
+void game_remove_tile_empty_position_fails(void** state);
+void game_remove_tile_out_of_bounds_fails(void** state);
+void game_remove_tile_with_meeple_fails(void** state);
+
+/* Tests pour scoring */
+void scoring_road_two_tiles_two_points(void** state);
+void scoring_town_incomplete_one_point(void** state);
+void scoring_town_with_blason_incomplete(void** state);
+void scoring_town_complete_flag_doubles_value(void** state);
+void scoring_field_zero_points(void** state);
+void scoring_village_zero_points(void** state);
+
+/* Tests pour engine_actions */
+void engine_wanted_action_is_place_tile_at_start(void** state);
+void engine_get_actions_not_empty_at_start(void** state);
+void engine_wanted_action_is_place_meeple_after_tile(void** state);
+void engine_get_actions_meeple_includes_none_option(void** state);
+void engine_get_actions_only_none_when_no_meeples(void** state);
+
+/* Tests pour engine_revert */
+void engine_revert_after_tile_removes_tile(void** state);
+void engine_revert_full_turn_clears_board(void** state);
+void engine_revert_partial_keeps_tile(void** state);
+void engine_revert_restores_player_index(void** state);
+void engine_revert_restores_meeple_count(void** state);
+void engine_revert_dispatch_vector_size(void** state);
+void engine_revert_current_hook_zero_on_empty(void** state);
+void engine_revert_two_turns_to_epoch_five(void** state);
+void engine_revert_end_game_state_restored(void** state);
+void engine_revert_then_dispatch_works(void** state);
+void engine_revert_long_play_with_revert(void** state);
+void engine_revert_interleaved_revert_and_play(void** state);
+
 static const struct CMUnitTest tests[] = {
     cmocka_unit_test(deck_builds),
     cmocka_unit_test(deck_seed_consistent),
@@ -172,6 +237,71 @@ static const struct CMUnitTest tests[] = {
     cmocka_unit_test(player_free_handles_null),
     cmocka_unit_test(player_has_meeple_returns_true),
     cmocka_unit_test(player_has_meeple_returns_false),
+
+    /* game_queries */
+    cmocka_unit_test(game_is_place_open_true_with_empty_neighbors),
+    cmocka_unit_test(game_is_place_open_false_when_all_neighbors_occupied),
+    cmocka_unit_test(game_is_space_available_true_for_empty),
+    cmocka_unit_test(game_is_space_available_false_when_occupied),
+    cmocka_unit_test(game_is_tile_placeable_null_game_returns_false),
+    cmocka_unit_test(game_is_tile_placeable_null_tile_returns_false),
+    cmocka_unit_test(game_get_current_player_returns_first_player),
+    cmocka_unit_test(game_get_current_player_advances_after_end_turn),
+
+    /* game_meeple */
+    cmocka_unit_test(game_place_meeple_success),
+    cmocka_unit_test(game_place_meeple_on_occupied_group_fails),
+    cmocka_unit_test(game_place_meeple_null_game_fails),
+    cmocka_unit_test(game_place_meeple_none_is_noop),
+    cmocka_unit_test(game_remove_meeple_success),
+    cmocka_unit_test(game_remove_meeple_empty_group_succeeds),
+    cmocka_unit_test(game_remove_meeple_null_game_fails),
+
+    /* game_turn */
+    cmocka_unit_test(game_end_player_turn_advances_player),
+    cmocka_unit_test(game_end_player_turn_at_last_returns_no_more_player),
+    cmocka_unit_test(game_end_round_resets_player_and_increments_turn),
+    cmocka_unit_test(game_end_round_before_last_player_fails),
+    cmocka_unit_test(is_game_finished_false_initially),
+    cmocka_unit_test(is_game_finished_false_with_no_turns_limit),
+    cmocka_unit_test(is_game_finished_true_when_turn_limit_reached),
+    cmocka_unit_test(game_end_player_turn_null_game_fails),
+    cmocka_unit_test(game_end_round_null_game_fails),
+
+    /* game_remove_tile */
+    cmocka_unit_test(game_remove_tile_success),
+    cmocka_unit_test(game_remove_tile_empty_position_fails),
+    cmocka_unit_test(game_remove_tile_out_of_bounds_fails),
+    cmocka_unit_test(game_remove_tile_with_meeple_fails),
+
+    /* scoring */
+    cmocka_unit_test(scoring_road_two_tiles_two_points),
+    cmocka_unit_test(scoring_town_incomplete_one_point),
+    cmocka_unit_test(scoring_town_with_blason_incomplete),
+    cmocka_unit_test(scoring_town_complete_flag_doubles_value),
+    cmocka_unit_test(scoring_field_zero_points),
+    cmocka_unit_test(scoring_village_zero_points),
+
+    /* engine_actions */
+    cmocka_unit_test(engine_wanted_action_is_place_tile_at_start),
+    cmocka_unit_test(engine_get_actions_not_empty_at_start),
+    cmocka_unit_test(engine_wanted_action_is_place_meeple_after_tile),
+    cmocka_unit_test(engine_get_actions_meeple_includes_none_option),
+    cmocka_unit_test(engine_get_actions_only_none_when_no_meeples),
+
+    /* engine_revert */
+    cmocka_unit_test(engine_revert_after_tile_removes_tile),
+    cmocka_unit_test(engine_revert_full_turn_clears_board),
+    cmocka_unit_test(engine_revert_partial_keeps_tile),
+    cmocka_unit_test(engine_revert_restores_player_index),
+    cmocka_unit_test(engine_revert_restores_meeple_count),
+    cmocka_unit_test(engine_revert_dispatch_vector_size),
+    cmocka_unit_test(engine_revert_current_hook_zero_on_empty),
+    cmocka_unit_test(engine_revert_two_turns_to_epoch_five),
+    cmocka_unit_test(engine_revert_end_game_state_restored),
+    cmocka_unit_test(engine_revert_then_dispatch_works),
+    cmocka_unit_test(engine_revert_long_play_with_revert),
+    cmocka_unit_test(engine_revert_interleaved_revert_and_play),
 
 };
 
