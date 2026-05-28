410fa28e3ce7ad1d462e78a499334659ed5823d8
Bastien VINCENT--DEKENS <bastien.vincent--dekens@telecomnancy.eu>
2026-05-28 23:24:22 +0000

    feat: mct implementation based on reworked pseudocode

--- lib/libai/tests/main.c
+++ lib/libai/tests/main.c
@@ -1,17 +1,3 @@
+#include <libai/tests/tests.h>
 
-#include <libai/libai.h>
-#include <libutils/cmocka.h>
-
-/* Test that the program does not fail when calling `libai_doing_stuff_works` */
-static void libai_doing_stuff_works(void** state) {
-  ai_do_stuff();
-  (void)state; /* unused */
-}
-
-int main(void) {
-  const struct CMUnitTest tests[] = {
-      cmocka_unit_test(libai_doing_stuff_works),
-  };
-
-  return cmocka_run_group_tests(tests, NULL, NULL);
-}
+int main(void) { return cmocka_run_group_tests(tests, NULL, NULL); }
---
+++ lib/libai/tests/tests.h
@@ -0,0 +1,27 @@
+#ifndef H_LIBAI_TESTS
+#define H_LIBAI_TESTS
+
+#include <libai/libai.h>
+#include <libcarcassonne/libcarcassonne.h>
+#include <libutils/cmocka.h>
+
+/* ai_play_turn */
+void ai_play_turn_does_not_crash(void** state);
+void ai_play_turn_completes_full_turn(void** state);
+void ai_play_turn_advances_current_player(void** state);
+void ai_play_turn_engine_ready_for_next_tile(void** state);
+void ai_play_turn_many_iterations_does_not_crash(void** state);
+void ai_play_turn_two_calls_advance_round(void** state);
+void ai_play_turn_full_game_two_players(void** state);
+
+static const struct CMUnitTest tests[] = {
+    cmocka_unit_test(ai_play_turn_does_not_crash),
+    cmocka_unit_test(ai_play_turn_completes_full_turn),
+    cmocka_unit_test(ai_play_turn_advances_current_player),
+    cmocka_unit_test(ai_play_turn_engine_ready_for_next_tile),
+    cmocka_unit_test(ai_play_turn_many_iterations_does_not_crash),
+    cmocka_unit_test(ai_play_turn_two_calls_advance_round),
+    cmocka_unit_test(ai_play_turn_full_game_two_players),
+};
+
+#endif
