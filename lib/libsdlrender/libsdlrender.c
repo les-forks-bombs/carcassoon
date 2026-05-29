50b21584baee0b3b6f502ce23067b6f9742d58e1
Matthieu Pignolet <matthieu@puffer.fish>
2026-05-30 01:04:41 +0200

    feat: format code and enable format checks in ci

--- lib/libsdlrender/libsdlrender.c
+++ lib/libsdlrender/libsdlrender.c
@@ -77,16 +77,16 @@ return_code_t run_sdl(engine_t* engine) {
 
   SDL_Event event;
   while (state.playing) {
-
-    if(game_get_current_player(&engine->game)->player_type==LIBCARCASSONNE_PLAYER_AI && !is_game_finished(&engine->game)){
+    if (game_get_current_player(&engine->game)->player_type ==
+            LIBCARCASSONNE_PLAYER_AI &&
+        !is_game_finished(&engine->game)) {
       ai_play_turn(engine, 20);
       get_current_actions(&state);
-    }else {
+    } else {
       while (SDL_PollEvent(&event)) {
         handle_app_event(&state, &event);
       }
     }
-
 
     SDL_SetRenderDrawColor(state.renderer, 0, 51, 153, 255);
     SDL_RenderClear(state.renderer);
