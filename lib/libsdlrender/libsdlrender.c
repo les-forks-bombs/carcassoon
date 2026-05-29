0208e7802389f778cd3183fc30f17f0d70275179
Salome P <salome.poirson@gmail.com>
2026-05-29 13:29:16 +0000

    feat(sdl): add background

--- lib/libsdlrender/libsdlrender.c
+++ lib/libsdlrender/libsdlrender.c
@@ -79,7 +79,7 @@ return_code_t run_sdl(engine_t* engine) {
       handle_app_event(&state, &event);
     }
 
-    SDL_SetRenderDrawColor(state.renderer, 164, 116, 73, 255);
+    SDL_SetRenderDrawColor(state.renderer, 0, 0, 0, 255);
     SDL_RenderClear(state.renderer);
 
     SDL_Rect map_viewport = {
