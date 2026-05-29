65a27bc3db5284c653bb816b416a73f93b6117f7
Matthieu Pignolet <matthieu@puffer.fish>
2026-05-30 00:08:24 +0200

    feat: implement search paths in order to copy GCC's DLLs

--- build/copy_dlls.sh
+++ build/copy_dlls.sh
@@ -9,23 +9,34 @@ fi
 # Le dossier ou on va enregistrer les DLLs (le dossier ou est l'exécutable)
 DEST=$(dirname "$(realpath "$EXE")")
 
-# On récupère le sysroot de mingw (le dossier avec les DLLs)
-SYSROOT="/usr/x86_64-w64-mingw32"
-MINGW_BIN="$SYSROOT/bin"
+GCC_LIB_DIR=$(dirname "$(x86_64-w64-mingw32-gcc -print-libgcc-file-name)")
+SEARCH_PATHS=(
+    "/usr/x86_64-w64-mingw32/bin"
+    "$GCC_LIB_DIR"
+)
 
-BUILTINS="^(kernel32|msvcrt|user32|gdi32|advapi32|shell32|ole32|ws2_32|setupapi|imm32|oleaut32|version|winmm|shlwapi|rpcrt4|comdlg32|ucrtbase)\.dll$"
+BUILTINS="^(kernel32|msvcrt|user32|gdi32|advapi32|shell32|ole32|ws2_32|setupapi|imm32|oleaut32|version|winmm|shlwapi|rpcrt4|comdlg32|ucrtbase|ntdll)\.dll$"
 
 copy_dependencies() {
     local target="$1"
-    
-    local dlls=$(x86_64-w64-mingw32-objdump -p "$target" | grep "DLL Name" | awk '{print $3}' |  grep -ivE $BUILTINS)
+    local dlls=$(x86_64-w64-mingw32-objdump -p "$target" 2>/dev/null | grep "DLL Name" | awk '{print $3}' | grep -ivE "$BUILTINS")
 
     for dll in $dlls; do
-        if [ -f "$MINGW_BIN/$dll" ]; then
-            if [ ! -f "$DEST/$dll" ]; then
-                cp "$MINGW_BIN/$dll" "$DEST/"
-                
-                copy_dependencies "$MINGW_BIN/$dll"
+        if [ ! -f "$DEST/$dll" ]; then
+            local found=0
+            
+            for path in "${SEARCH_PATHS[@]}"; do
+                if [ -f "$path/$dll" ]; then
+                    echo "Copying $dll from $path..."
+                    cp "$path/$dll" "$DEST/"
+                    found=1
+                    copy_dependencies "$path/$dll"
+                    break
+                fi
+            done
+            
+            if [ $found -eq 0 ]; then
+                echo "Warning: Could not find $dll in search paths."
             fi
         fi
     done
