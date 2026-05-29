2793e9456764ed6ddfff4e511a720f2f4dd2a6ce
Damien Jung <damien.jung@telecomnancy.eu>
2026-05-29 11:39:45 +0200

    fix(libsdlrender): Fixing possibles_meeples not reinitialized

--- lib/libsdlrender/meeple.c
+++ lib/libsdlrender/meeple.c
@@ -7,7 +7,9 @@
 #include <libsdlrender/consts.h>
 #include <libsdlrender/forward.h>
 #include <libsdlrender/meeple.h>
+#include <stdbool.h>
 #include <string.h>
+#include "libcarcassonne/macros.h"
 
 static SDL_FRect calc_meeple_rect(tile_slot_t slot, const SDL_FRect *tile_rect,
                                   double angle) {
@@ -118,12 +120,14 @@ void render_possible_meeples(placed_tile_t *tile, appstate_t *as,
 }
 
 void update_possible_meeples(appstate_t *as) {
+
+  memset(as->possible_meeples, 0, SIZE(as->possible_meeples)*sizeof(bool));
+
   for (unsigned int i = 0; i < vector_size(&as->all_actions); i++) {
     action_t *action = vector_nth(&as->all_actions, i);
-    if (action->type == LIBCARCASSONNE_ACTION_PLACE_MEEPLE &&
-        action->order.place_meeple.meeple_type != NONE) {
-      int group                   = action->order.place_meeple.part_group;
-      as->possible_meeples[group] = true;
-    }
+    int       group  = action->order.place_meeple.part_group;
+    as->possible_meeples[group] =
+        (action->type == LIBCARCASSONNE_ACTION_PLACE_MEEPLE &&
+         action->order.place_meeple.meeple_type != NONE);
   }
 }
