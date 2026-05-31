a894857277f94da05a8a0c63846783223d5bd4ff
Salomé <salome.poirson@telecomnancy.eu>
2026-05-31 09:48:28 +0000

    feat(sdl): documentation

--- lib/libsdlrender/camera.h
+++ lib/libsdlrender/camera.h
@@ -5,11 +5,19 @@
 
 #include "libcarcassonne/enums.h"
 
+/// @brief Représente une caméra pour l'affichage de la carte
 struct camera {
+  /// @brief La position de la caméra
   float x, y;
   float zoom;
 };
 
+/// @brief Permet de créer une instance d'une caméra
+/// @return Le code de statut, SUCCESS si l'opération a réussi, autre sinon
 return_code_t create_camera(camera_t *camera);
+/// @brief Met à jour l'affichage en fonction de la position de la caméra
+/// @param state L'affichage de la partie en cours
 void          update_camera(appstate_t *state);
+/// @brief Centre la caméra autour de la tuile de départ
+/// @param state L'affichage de la partie en cours
 void          center_camera_on_start(appstate_t *state);
--- lib/libsdlrender/text.h
+++ lib/libsdlrender/text.h
@@ -4,17 +4,24 @@
 #include <SDL3_ttf/SDL_ttf.h>
 #include <libsdlrender/forward.h>
 
+/// @brief Représente un objet texte pour l'affichage
 struct text_object {
+  /// @brief La texture associée à l'objet
   SDL_Texture *texture;
+  /// @brief La taille de la police
   float        font_size;
+  /// @brief La couleur du texte
   SDL_Color    color;
+  /// @brief Le texte affiché
   char        *content;
+  /// @brief La position du texte
   float        w, h;
 };
 
+/// @brief Permet de créer une instance d'objet texte
 void text_object_create(appstate_t *appstate, text_object_t *text,
                         const char *content, SDL_Color color, float font_size);
-
+/// @brief Permet de mettre à jour l'affichage d'un objet texte
 void text_object_update(appstate_t *appstate, text_object_t *to);
-
+/// @brief Permet de désallouer un objet texte
 void text_object_free(text_object_t *text);
